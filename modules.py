e59de680689fdcee390bc2414b0906ef2b741cfd
voxeltiramisu <saaniwuntiya@gmail.com>
2026-03-06 19:00:53 -0500

    Updating modules
    Adding basic doc strings and more structure

---
+++ modules.py
@@ -0,0 +1,32 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import time 
+import neurokit2 as nk 
+from scipy import signal
+
+
+
+
+def data_loader():
+    """Loads data from two headers in csv into a into numpy arrays."""
+    return
+
+def preprocessing():
+    """
+    Handles missing/corrupted values, normalization, baseline,
+    correction where appropriate and datatype selection
+    """
+    return
+
+def filters():
+    """"filter implementation lpf, hpf, bpf, FIR, IIR filters"""
+    return
+
+def analysis():
+    """"FFT of signal used to display frequency domain function"""
+    return
+
+def gui():
+    """"Interactive time and frequency domain display for all headers."""
+    return
